8c5e425501f3bf90f77a677c01903ffb44f7d0e3
armandoep <edwin_perez2010@hotmail.es>
2020-03-21 12:23:25 -0600

    Enviado mis archivos al repositorio remoto

---
+++ src/pages/Error404.js
@@ -0,0 +1,11 @@
+const Error404 = () => {
+  const view = `
+    <div class="Error404">
+      <h2>Created with THE RICK AND MORTY API</h2>
+      <a>https://rickandmortyapi.com/</a>
+    </div>
+  `;
+  return view;
+};
+
+export default Error404;
