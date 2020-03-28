c5c53877dfa772a39f7911d8dc48545b06b63064
armandoep <60561840+armandoep@users.noreply.github.com>
2020-03-28 15:45:26 -0600

    Update Header.js

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="/SPA-JS-Vanilla/">
             <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.m.wikipedia.org%2Fwiki%2FArchivo%3ARick_and_Morty.svg&psig=AOvVaw2wnNC8gkyArrQcNyeMDiC9&ust=1585518069834000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiXuZuRvugCFQAAAAAdAAAAABAH' class='rick-logo'>
           </a>
         </h1>
