e7369179c209c20b597abff1a929fb2161a8adae
armandoep <60561840+armandoep@users.noreply.github.com>
2020-03-28 15:42:28 -0600

    Update Header.js

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
       <div class="Header-logo">
         <h1>
           <a href="/">
-            <img src='./Rick_and_Morty.svg' class='rick-logo'>
+            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.m.wikipedia.org%2Fwiki%2FArchivo%3ARick_and_Morty.svg&psig=AOvVaw2wnNC8gkyArrQcNyeMDiC9&ust=1585518069834000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiXuZuRvugCFQAAAAAdAAAAABAH' class='rick-logo'>
           </a>
         </h1>
       </div>
